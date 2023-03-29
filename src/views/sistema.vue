e680798048b20b1658a0043562082d515aac6155
trompetatrompetista <128755103+trompetatrompetista@users.noreply.github.com>
2023-03-29 13:40:29 -0300

    Add files via upload

--- src/views/sistema.vue
+++ src/views/sistema.vue
@@ -1,73 +1,68 @@
 <template>
 
-<img alt="Vue logo" src="https://img.freepik.com/vector-gratis/panda-lindo-trabajando-ilustracion-icono-vector-dibujos-animados-portatil-concepto-icono-tecnologia-animal-aislado-vector-premium-estilo-dibujos-animados-plana_138676-3487.jpg?w=360&t=st=1679750829~exp=1679751429~hmac=592fc4482507a38d4ce3626e6e306ad7038889d8e031f64347e89e76333e4b40">
-    <div class="hello">
-      <h1>{{ msg }}</h1>
-      <p>
-        bueno
-        <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-      </p>
-      <h3>intalar</h3>
-     <img class="hola1" src="https://picsum.photos/300/300?grayscale">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero repudiandae, natus, omnis quidem nisi amet pariatur deserunt, nesciunt dolore provident corrupti consectetur ex distinctio eius at. Ducimus iusto soluta numquam?       
-      <ul>
-        <li><a href="https://www.google.com/?hl=es" target="_blank" rel="noopener">babel</a></li>
-        <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-        <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      </ul>
-      <h3>que pensar de todo esto</h3>
-      <div class="hola"> <div class="foto2"><img src="https://picsum.photos/300/300?grayscale"></div><div class="texto2"><h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero repudiandae, natus, omnis quidem nisi amet pariatur deserunt, nesciunt dolore provident corrupti consectetur ex distinctio eius at. Ducimus iusto soluta numquam?</h1> </div> </div>
-      <ul>
-        <li><a href="https://www.google.com/?hl=es" target="_blank" rel="noopener">google</a></li>
-        <li><a href="https://www.google.com/?hl=es" target="_blank" rel="noopener">vospensabas que solo podia porner a google mira esto</a></li>
-        <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-        <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-        <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-        <img src="https://picsum.photos/200/300?grayscale">
-      </ul>
-      <h3>megustan laschicas me gustan la jodas me gustan</h3>
-      <img src="https://picsum.photos/id/300/300?grayscale">
-      <img src="https://picsum.photos/200/300/?blur=2?">
-      <ul>
-        <li><a href="https://www.google.com/?hl=es" target="_blank" rel="noopener">y esto..</a></li>
-        <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-        <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-        <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-        <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-      </ul>
-    </div>
+<h2>Accordion</h2>
+
+<button class="accordion">Section 1</button>
+<div class="panel">
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+</div>
+
+<button class="accordion">Section 2</button>
+<div class="panel">
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+</div>
+
+<button class="accordion">Section 3</button>
+<div class="panel">
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+</div>
+
+
   </template>
   
   <script>
-  export default {
-    name: 'HelloWorld',
-    props: {
-      msg: String
+ var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
     }
-  }
+  });
+}
   </script>
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped lang="scss">
-  h3 {
-    margin: 40px 0 0;
-  }
-  ul {
-    list-style-type: kannada;
-    padding: 0;
-  }
-  li {
-    list-style-type: kannada;
-    display: inline-block;
-    margin: 0 10px;
-  }
-  a {
-    color: #582036;
-  }
- img.hola1{ 
-  vertical-align: text-top;  
+  <style >
+ 
+ .accordion {
+  background-color: #eee;
+  color: #444;
+  cursor: pointer;
+  padding: 18px;
+  width: 100%;
+  border: none;
+  text-align: left;
+  outline: none;
+  font-size: 15px;
+  transition: 0.4s;
 }
 
+.active, .accordion:hover {
+  background-color: #ccc; 
+}
 
+.panel {
+  padding: 0 18px;
+  display: none;
+  background-color: white;
+  overflow: hidden;
+}
 
    
 
